c6d125b74a54e0b9c553928c087ceaf83f9e298c
aydoganuyanikoglu <aydogan.u18@gmail.com>
2025-03-16 18:37:58 +0300

    u

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -4,7 +4,10 @@ import { sql } from "@vercel/postgres";
 export async function GET(_req: NextRequest) {
   try {
     const result = await sql.query(`SELECT * FROM products`);
-    return NextResponse.json({ products: result.rows }, { status: 200 });
+    return NextResponse.json(
+      { case: "success", products: result.rows },
+      { status: 200 }
+    );
   } catch (error: any) {
     return NextResponse.json(
       { error: "Internal Server Error", details: error.message },
